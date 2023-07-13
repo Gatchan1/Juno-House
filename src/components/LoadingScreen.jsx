e9b26f059ae01c3e7f110b3b63fc74d7560d61ec
Raquel Barrio <r.barrio.f@gmail.com>
2023-07-13 13:23:07 +0200

    deploy?

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,7 +1,7 @@
 export default function LoadingScreen() {
   return (
     <div id="LoadingScreen">
-    <img src="logo.png" alt="Juno House logo" />
+    <img src="https://github.com/Gatchan1/Juno-House/blob/master/dist/logo.png" alt="Juno House logo" />
     </div>
   )
 }
